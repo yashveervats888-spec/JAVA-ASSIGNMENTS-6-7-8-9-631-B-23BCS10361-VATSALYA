19dc9a276ee05e4f1a17020af5ba489697e2a58c
singhvatsalya <143573093+singhvatsalya@users.noreply.github.com>
2025-11-04 15:37:56 +0530

    7.1-1 23BCS10361.java

---
+++ 7.1-1 23BCS10361.java
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <form action="login" method="post">
+        Username: <input type="text" name="username"><br><br>
+        Password: <input type="password" name="password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
